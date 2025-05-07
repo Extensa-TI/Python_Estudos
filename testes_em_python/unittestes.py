31a7444dc03cb7d27617867a635c79ce3e0f17c9
Extensa-TI <extensati@gmail.com>
2025-05-07 20:31:04 -0300

    Tipos de assertions

--- testes_em_python/unittestes.py
+++ testes_em_python/unittestes.py
@@ -1,2 +1,14 @@
 """
+assertEqual(a,b)        -> a == b
+assertNotEqual(a,b)     -> a != b
+assertTrue(x)           -> x é true
+assertFalse(x)          -> x é false
+assertIs(a,b)           -> a é b
+assertIsNot(a,b)        -> a não é b
+assertIfNone(x)         -> x é None
+assertIfNotNone(x)      -> x não é None
+assertIn(a,b)           -> a está em b
+assertNotIn(a,b)        -> a não está em b
+assertIsInstance(a,b)   -> se a é instância de b
+assertNotIsInstance(a,b)-> se a não é instância de b
 """
