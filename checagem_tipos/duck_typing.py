932355a2a52c723747626642bbb12d870f1b4f41
Extensa-TI <extensati@gmail.com>
2025-05-29 15:12:23 -0300

    Duck Tiping

---
+++ checagem_tipos/duck_typing.py
@@ -0,0 +1,11 @@
+"""
+    Se um objeto, anda como pato, come como pato, nada como um pato, é pato.
+"""
+
+class CisneNegro:
+
+    def __len__(self):
+        return 42
+
+livro = CisneNegro()
+print(len(livro))
